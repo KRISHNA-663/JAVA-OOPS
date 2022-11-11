7d7654e4f078b003e2236f14db0295e58b010490
KRISHNA <krish7cbe@gmail.com>
2022-11-11 17:51:30 +0530

    Multi-Dimensional Array

---
+++ Problems/2DimensionalArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+		int a[][]=new int[3][3];
+		for(int i=0;i<3;i++)
+		{
+		    for(int j=0;j<3;j++)
+		    {
+		        a[i][j]=s.nextInt();
+		        
+		    }
+		    
+		}
+		for(int i=0;i<3;i++)
+		{
+		    for(int j=0;j<3;j++)
+		    {
+		        System.out.print(a[i][j]+" ");
+		    }
+		    System.out.println(" ");
+		}
+		
+	}
+}
+/*
+input - 1 2 3 4 5 6 7 8 9
+output - 
+1 2 3  
+4 5 6  
+7 8 9  
